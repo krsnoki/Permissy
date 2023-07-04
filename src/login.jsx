be3d3a8bc7472e8009ecc52892b1b434d9901d98
Kalyani <kalyanikolte0@gmail.com>
2023-07-04 13:57:15 +0530

    landing page, login page, initial css, no-media-query, changes ongoing

--- src/login.jsx
+++ src/login.jsx
@@ -1,16 +1,53 @@
+<script src="http://localhost:8097"></script>
+
 import './styles/global.css';
 import './styles/login.css';
+import { useFormik } from 'formik';
 
 function Login() {
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: ''
+        },
+        onSubmit: values => {
+            console.log('Form values: ', values);
+        }
+    })
+
     return (
+    
         <div className='login-container'>
-        <div className='heading'>Login</div>
             <div className='login-area'>
-                <div className='loginhead'></div>
-                <div className='form'></div>
+                <div className='loginhead'> <h1>Faculty Login</h1></div>
+                <hr/>
+                <form onSubmit={ formik.handleSubmit }> 
+                    <label 
+                    htmlFor='username'>Username: 
+                </label>
+                <input 
+                    type = 'text' 
+                    name='username'  
+                    onChange={ formik.handleChange}  
+                    value= {formik.values.username }/>
+                
+                <label 
+                    htmlFor='password'>Password: 
+                </label>
+                
+                <input 
+                    type='password' 
+                    name='password' 
+                    onChange={ formik.handleChange } 
+                    value={ formik.values.password }
+                />
 
-            </div>
+                    <button type='submit' name='submit'>submit</button>
+                </form>
+                <div className='fpass'><h5><a href='#'>Forgot Password ?</a></h5></div>
 
+            </div> 
         </div>
     )
 }
