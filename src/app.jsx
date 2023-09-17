f57b3ef3382e7ce3f5cf0ddc437d08b2b60aae47
Aashutosh-922 <karaleash462@gmail.com>
2023-09-18 00:09:43 +0530

    cmt

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,47 @@
+// import Hero from './pages/hero'
+// import Footer from './components/footer'
+// import Navbar from './components/navbar'
+// import About from './pages/About'
+// import { Router, Routes, Route } from 'react-router-dom'
+// import Login from './pages/login'
+
+// function App() {
+
+//     return (
+//         <Router>
+//             <Navbar />
+//             <Hero />
+//             <Routes>
+//                 <Route exact to='/#about' element={<About />} />
+//                 <Route exact to= '#login' element= {<Login/>}/>
+//                 {/* <Login /> */}
+//             </Routes>
+
+//             <Footer />
+//         </Router>
+//     )
+// }
+
+// export default App
+
+
 import Hero from './pages/hero'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 import About from './pages/About'
-import { Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 
 function App() {
-
     return (
-        <Router>
-            <Navbar />
+        <div>
+            <Navbar/>
             <Hero />
-            <Routes>
-                <Route exact to='/#about' element={<About />} />
-                <Route exact to= '#login' element= {<Login/>}/>
-                {/* <Login /> */}
-            </Routes>
-
+            <About/>
             <Footer />
-        </Router>
+    </div>
     )
 }
 
 export default App
+
